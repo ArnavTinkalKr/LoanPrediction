aed12919b34ace8cb19f8ba976fb11d825e48f20
Tinkal kumar <arnav7549@gmail.com>
2023-04-16 21:45:18 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 # save this as app.py
 # pylint: disable=unused-import
-import sklearn
-from flask import Flask, escape, request, render_template
-import pickle
 import numpy as np
+import sklearn
+from flask import Flask, request, render_template
+import pickle
 import joblib
+
 
 model = joblib.load('/home/tinkal/Desktop/LoanPredictionProject/model.pkl')
 
-
+# model1 = joblib.load('/home/tinkal/Desktop/LoanPredictionProject/model1.pkl')
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return render_template("index.html")
-
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
@@ -87,7 +86,6 @@ def predict():
             semiurban=0
             urban=0
 
-
         ApplicantIncomelog = np.log(ApplicantIncome)
         totalincomelog = np.log(ApplicantIncome+CoapplicantIncome)
         LoanAmountlog = np.log(LoanAmount)
@@ -111,8 +109,79 @@ def predict():
 
     else:
         return render_template("prediction.html")
+    
+# define route for the about page
+@app.route('/about')
+def about():
+    return render_template('aboutus.html')
 
+
+"""
+# define route to handle loan calculation
+@app.route('/calculate', methods=['GET', 'POST'])
+def calculate():
+    if request.method == 'POST':
+        # validate loan data from form
+        p = request.form.get("PrincipalAmount")
+        R = request.form.get("AnnualInterestRate")
+        n = request.form.get("EntarNumberOfMounth")
+
+        if p and R and n:
+            # All form fields are present and non-empty
+            p = float(p)
+            R = float(R)
+            n = int(n)
+
+            # calculate interest rate per month
+            r = R / (12 * 100)
+
+            # calculate equated monthly installment (EMI)
+            emi = p * r * ((1+r)**n) / ((1+r)**n - 1)
+            
+            # determine loan status based on EMI
+            if emi <= 0:
+                status = "Invalid input"
+            elif emi <= 10000:
+                status = "Approved"
+            else:
+                status = "Rejected"
+            
+        # render result template with EMI and prediction
+        #return render_template("result.html", result_text="EMI is {} and loan status is {}".format(emi, status))
+    else:
+        return render_template("result.html")
+
+"""
+
+
+@app.route('/calculate', methods=['GET', 'POST'])
+def calculate():
+    if request.method == 'POST': 
+       loan_amount = int(request.form.get('loan_amount'))
+       tenure = int(request.form.get('tenure'))
+       interest_rate = float(request.form.get('interest_rate'))
+       monthly_interest_rate = interest_rate/(12*100)
+       monthly_emi = (loan_amount*monthly_interest_rate*((1+monthly_interest_rate)**tenure))/(((1+monthly_interest_rate)**tenure)-1)
+       total_amount_payable = monthly_emi*tenure
+
+       
+       #return render_template("result.html", result_text="monthly_emi is {}, loan_amount is {}, total_amount_payable is {}".format(round(monthly_emi, 2), loan_amount, round(total_amount_payable, 2)))
+       #return render_template("result.html", result_text="EMI is {} and loan status is {}.format(emi, status))
+    
+    
+       return render_template("result.html", emi_text ="Monthly EMI is: {}".format(round(monthly_emi, 2)), loanamount_text ="Loan Amount Is: {}".format(loan_amount), totalpayble_text ="Total Amount Pyable Is: {}".format(round(total_amount_payable, 2)))
+    return render_template("result.html")
+    
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
+
+
+
+
+
+
+
+ 
+ 
